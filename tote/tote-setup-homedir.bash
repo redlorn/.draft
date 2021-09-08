47662a505cf55e39c575db20961665c48f309244
redlorn <redlorn@protonmail.com>
2021-09-08 12:50:04 -0700

    homedir setup script wip

---
+++ tote/tote-setup-homedir.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit -o pipefail -o privileged -o nounset
+shopt -s extglob
+
+##
+# Makes a tote style home directory
+
+main () {
+    local -a HOME_DIRS
+    mapfile -rta HOME_DIRS <<< tote.homedirs.txt
+    
+    # create and chown/chmod home sub-directories
+    for subdir in "${HOME_DIRS[@]}" ; do
+        local path
+        path="$HOME/$subdir" 
+
+        [ -d "$path" ] ||
+            mkdir -p "$path"
+        
+        # entire directory tree owned by user
+        chown -r "$USER" "$USER" "$path"
+
+        # remove all public permissions except for the public directory 
+        if [[ "$path" == "public" ]] ; then
+            chmod -r o-w "$path"
+        else
+            chmod -r o-rwx "$path"
+        fi
+    done
+    
+    exit 0
+}
+
+main
